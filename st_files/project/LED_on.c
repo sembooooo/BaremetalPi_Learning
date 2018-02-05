d932763a41ddab7b8ad6fd560768b334a42553ad
sembooooo <srikarsana1995@gmail.com>
2018-02-05 07:57:57 +0530

    Update LED_on.c

--- st_files/project/LED_on.c
+++ st_files/project/LED_on.c
@@ -2,10 +2,22 @@
 
 int main()
 {
+		/* Enable Clock for the GPIOD port*/
+		RCC->AHB1ENR|=(1<<3);
+	        
+	        /* Configure the GPIO port for desired settings */
 		GPIO_setmode(GPIOD,13,OUTPUT);
 		GPIO_setpupdr(GPIOD,13,PULLUP);
-	  GPIO_setodr(GPIOD,(1<<13));
+	        GPIO_setodr(GPIOD,(1<<13));
 		while(1);
+
+
+	
 }
+
+
+
+
+
 
 
