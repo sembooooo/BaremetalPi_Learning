0b0086ba419668e1d23fbd13ce3c03dc45d157bc
sana srikar <srikarsana1995@gmail.com>
2019-03-04 14:45:59 +0530

    Update OS_header.h

--- st_files/project/TOY_OS/OS_header.h
+++ st_files/project/TOY_OS/OS_header.h
@@ -2,7 +2,6 @@
 #define OS_HEADER_H_INCLUDED
 
 extern void OS_Launch(uint32_t timeslice);
-extern void OS_Init();
 extern uint32_t OS_AddThreads(void(*task0)(void), void(*task1)(void), void(*task2)(void));
 
 typedef enum
@@ -11,4 +10,8 @@ typedef enum
   E_ERROR = 1 
 } err_status_N;
 
+static inline void OS_Init()
+{
+		__asm( "CPSID I ");  /* Disable interrupts */
+}
 #endif
