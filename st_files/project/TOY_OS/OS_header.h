5159840220d5daad501cbd7d3272c1887598559d
sana srikar <srikarsana1995@gmail.com>
2019-03-04 14:40:58 +0530

    Update OS_header.h

--- st_files/project/TOY_OS/OS_header.h
+++ st_files/project/TOY_OS/OS_header.h
@@ -1,4 +1,14 @@
-/* this header file contains all the necessary function declarations*/3
+#ifndef OS_HEADER_H_INCLUDED
+#define OS_HEADER_H_INCLUDED
+
 extern void OS_Launch(uint32_t timeslice);
 extern void OS_Init();
 extern uint32_t OS_AddThreads(void(*task0)(void), void(*task1)(void), void(*task2)(void));
+
+typedef enum
+{
+  E_OK    = 0,
+  E_ERROR = 1 
+} err_status_N;
+
+#endif
